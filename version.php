6dca77a8b50a3b9221eddc36bcd766b94284f4d2
sumanbogati <sumanbogati@gmail.com>
2016-02-26 14:14:04 +0530

    update version

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016021500;      // The current module version (Date: YYYYMMDDXX).
-$plugin->release = '2.1 (Build: 2016021500)';
+$plugin->version   = 2016022600;      // The current module version (Date: YYYYMMDDXX).
+$plugin->release = '2.2 (Build: 2016022600)';
 $plugin->requires  = 2014051202;      // Requires this Moodle version.(2.7.2+)
 $plugin->cron      = 0;               // Period for cron to check this module (secs).
 $plugin->component = 'mod_congrea'; // To check on upgrade, that module sits in correct place.
