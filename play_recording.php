0316deb55ab64b2ab0d1814df493e347745d32bc
ravi18011991 <ravi.kumar@vidyamantra.com>
2018-06-09 16:32:40 +0530

    fix play recording issue

--- play_recording.php
+++ play_recording.php
@@ -21,7 +21,6 @@
  * @copyright 2015 Pinky Sharma
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-defined('MOODLE_INTERNAL') || die();
 cors();
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 
