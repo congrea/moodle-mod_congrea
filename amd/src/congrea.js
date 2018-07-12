4bedb2a16759a053bd909dfe11d4c36eead48d3f
ravi18011991 <ravi.kumar@vidyamantra.com>
2018-07-12 17:09:45 +0530

    indent congrea.js

--- amd/src/congrea.js
+++ amd/src/congrea.js
@@ -9,18 +9,18 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-define(['jquery', 'core/ajax', 'core/notification'], function ($){
+define(['jquery', 'core/ajax', 'core/notification'], function ($) {
     return {
-        presetcolor: function (){
-            $(".form-select.defaultsnext #id_s_mod_congrea_preset").change(function (){
+        presetcolor: function () {
+            $(".form-select.defaultsnext #id_s_mod_congrea_preset").change(function () {
                 var val = this.value;
                 $('.admin_colourpicker .currentcolour').css('background-color', val);
                 $('#id_s_mod_congrea_colorpicker').val(val);
             });
 
         },
-        congrea_online_popup: function (){
-            $('#overrideform').submit(function (){
+        congrea_online_popup: function () {
+            $('#overrideform').submit(function () {
                 var newTab = window.open('', 'popupVc');
                 if (window.newTab && window.newTab.closed === false) {
                     newTab.focus();
@@ -34,8 +34,8 @@ define(['jquery', 'core/ajax', 'core/notification'], function ($){
                 return newTab;
             });
         },
-        congrea_play_recording: function (){
-            $('.playAct').submit(function (){
+        congrea_play_recording: function () {
+            $('.playAct').submit(function () {
                 var newTab = window.open('', 'popupVc');
                 if (window.newTab && window.newTab.closed === false) {
                     newTab.focus();
