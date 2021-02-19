fbd42546da90a4d7b3ca10fc1657847c5b33716d
Manisha Dayal <manisha.dayal@vidyamantra.com>
2021-02-19 10:32:58 +0530

    Congrea report_db version 2021021900 upgraded

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021091507; // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2021021900; // The current module version (Date: YYYYMMDDXX).
 $plugin->release = 'v3.5+r8a/b'; // Changes specific to storing reports in db
 $plugin->requires = 2018051700;      // Requires this Moodle version.(3.5+).
 $plugin->cron = 0;               // Period for cron to check this module (secs).
